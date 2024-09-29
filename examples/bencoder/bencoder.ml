a27be93eacbd80fff519075616761eac04bdf465
theteachr <sop.theteachr@gmail.com>
2024-09-30 02:01:37 +0530

    Use `_`'ed names

--- examples/bencoder/bencoder.ml
+++ examples/bencoder/bencoder.ml
@@ -38,29 +38,24 @@ let byte_string =
   char ':' *> take n
 
 let integer = bracket (char 'i') number (char 'e')
+let b_integer = lift num integer
+let b_byte_string = lift str byte_string
 
-(* Do we really need to fail if the keys are not sorted? Doesn't seem like a
-   responsibility of the parser. *)
-let sorted_keys pairs =
-  let keys = List.map fst pairs in
-  if sorted keys then pure pairs else fail "not sorted"
-
-let rec bvalue input = (bint <|> bstring <|> blist <|> bdict) input
-and bint = lift num integer
-and bstring = lift str byte_string
+let rec b_value input =
+  (b_integer <|> b_byte_string <|> b_list <|> b_dict) input
 
 (* XXX: Parse [t list] instead of [Lizt]? *)
-and blist input =
-  let p = bracket (char 'l') (many bvalue) (char 'e') in
+and b_list input =
+  let p = bracket (char 'l') (many b_value) (char 'e') in
   lift lizt p input
 
 (* Can we do something about [lift _ p input] pattern (looks ugly)? *)
 
 (* XXX: Parse [(string * t) list] instead of [Dict]? *)
-and bdict input =
+and b_dict input =
   let entry =
     let* key = byte_string in
-    let* value = bvalue in
+    let* value = b_value in
     pure (key, value)
   in
   let p = bracket (char 'd') (many entry) (char 'e') in
@@ -68,21 +63,21 @@ and bdict input =
 
 (* TODO: Improve tests *)
 let%test "bencoding int" =
-  "i42e" |> Input.create |> bvalue |> Result.map fst = Ok (Num 42)
+  "i42e" |> Input.create |> b_value |> Result.map fst = Ok (Num 42)
 
 let%test "bencoding string" =
-  "5:hello" |> Input.create |> bvalue |> Result.map fst = Ok (Str "hello")
+  "5:hello" |> Input.create |> b_value |> Result.map fst = Ok (Str "hello")
 
 let%test "bencoding homogeneous list" =
   "l4:spami42ee"
   |> Input.create
-  |> bvalue
+  |> b_value
   |> Result.map fst
   = Ok (Lizt [ Str "spam"; Num 42 ])
 
 let%test "bencoding dictionaries" =
   "d3:foo3:bar4:spami42ee"
   |> Input.create
-  |> bvalue
+  |> b_value
   |> Result.map fst
   = Ok (Dict [ ("foo", Str "bar"); ("spam", Num 42) ])
